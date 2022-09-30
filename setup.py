61a2947b380130d5b5d2ef54a861f36dbdd0e11b
Kelly Tran <kelly.tran099@gmail.com>
2022-09-30 11:14:35 -0400

    fixing yaml

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pioreactor_relay_plugin",
-    version="0.0.7",
+    version="0.0.8",
     license="MIT",
     description="Turn your additional hardware (light source, pump) on or off.",
     long_description=open("README.md").read(),
